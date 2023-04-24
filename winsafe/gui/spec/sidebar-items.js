5e40c921780e470ee016052451864e17c3edc6af
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-04-23 21:59:58 -0300

    Cargo doc auto deployment 2023-04-23 21:59:57.

--- winsafe/gui/spec/sidebar-items.js
+++ winsafe/gui/spec/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["ComboBoxItems","ListBoxItems","ListViewColumns","ListViewItem","ListViewItems","StatusBarParts","TabItem","TabItems","TreeViewItem","TreeViewItems"]};
+window.SIDEBAR_ITEMS = {"struct":["ComboBoxItems","ListBoxItems","ListViewColumn","ListViewColumns","ListViewItem","ListViewItems","StatusBarParts","TabItem","TabItems","TreeViewItem","TreeViewItems"]};
